8d7398d99beeabc666428df94fdfae4c793651af
nnspecter <nevozmozhno0000@email.com>
2025-07-26 18:09:27 +0300

    добавил кастомный хук useFetch

--- src/components/customHooks/useFetch.jsx
+++ src/components/customHooks/useFetch.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
-const useFetch = ({url}) => {
-  return (
-    <div>useFetch</div>
-  )
+import load_ico from "../../assets/loading.gif"
+import { useEffect, useState } from 'react';
+const useFetch = (url) => {
+    const [data, setData] = useState({products: []});
+    const[loading, setLoading] = useState(false);
+    const[error, setError] = useState('');
+    useEffect(()=>{
+        const fetchData = async () => {
+            setLoading(true);
+            setError('');
+            try{
+                const response =  await fetch(url);
+                if(!response.ok){
+                    throw new Error("Ошибка на сервере");
+                }
+                const result = await response.json();
+                setData(result);
+            }
+            catch(err){
+                setError(err.message);
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        fetchData();
+    },[url])
+   
+   return {data, loading, error}
 }
 
 export default useFetch
--- src/components/item-board/ItemBoard.jsx
+++ src/components/item-board/ItemBoard.jsx
@@ -1,59 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import Item from '../item/item'
+import Item from '../item/Item'
 import "./ItemBoard.css"
+import useFetch from '../customHooks/useFetch';
 const ItemBoard = () => {
-  const[loading, setLoading] = useState(false);
-  const[error, setError] = useState('');
   const[itemsData, setItemsData] = useState({products: []});
 
 
   //api товаров 
   const url = "https://dummyjson.com/products"
-  console.log(itemsData.products[1])
-  useEffect(()=>{
-    const fetchItems = async () =>{
-      setLoading(true);
-      setError('');
-      try{
-        const response = await fetch(url);
-        if(!response.ok){
-          throw new Error("Ошибка на сервере");
-        }
-        const data = await response.json();
-        setItemsData(data);
-      }
-      catch(err){
-        setError(err.message);
-      }
-
-      finally{
-        setLoading(false)
-      }
-      }
-    fetchItems();
-  }, [])
+  const {data, loading, error} = useFetch(url);
 
   return (
     <div className='ItemBoard'>
       <div className='BoardContainer'>
-        {itemsData.products.slice(0, 150).map((item) => (
+        {
+          data.products.slice(0, 150).map((item) => (
           <Item
             key={item.id}
+            id={item.id}
             image={item.images[0]}
             title={item.title}
             rating={item.rating}
             price={item.price}
           />
-        ))}
-        {itemsData.products.slice(0, 14).map((item) => (
-          <Item
-            key={item.id}
-            image={item.images[0]}
-            title={item.title}
-            rating={item.rating}
-            price={item.price}
-          />
-        ))}
+        ))
+        }
+        
       </div>
     </div>
   )
